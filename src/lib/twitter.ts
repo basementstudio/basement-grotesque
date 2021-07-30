f328bd974728aff03932d6f604c4c7b3ec0266aa
Matias Perez <matiasperezpc@hotmail.com>
2021-07-30 12:38:32 -0300

    Adding twitter typings

--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -1,7 +1,23 @@
+import { Status } from 'twitter-d'
+
 const TWITTER_HASHTAG = '#basementgrotesque'
 
+type TwitterRes = {
+  statuses: Status[]
+  search_metadata: {
+    completed_in: number
+    max_id: number
+    max_id_str: string
+    next_results: string
+    query: string
+    count: number
+    since_id: number
+    since_id_str: string
+  }
+}
+
 export const getHashtagTweets = async () => {
-  const res = await fetch(
+  const res = (await fetch(
     `https://api.twitter.com/1.1/search/tweets.json?q=${encodeURIComponent(
       TWITTER_HASHTAG
     )}`,
@@ -10,7 +26,7 @@ export const getHashtagTweets = async () => {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
       }
     }
-  ).then((res) => res.json())
+  ).then((res) => res.json())) as TwitterRes
 
   return res
 }
