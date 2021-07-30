2057bad09de0080ac5d97fdec41a815554a04b4f
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2021-07-30 19:02:21 -0300

    feat: about update

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -10,9 +10,10 @@ const Container = styled('div', {
         backgroundColor: '$white'
       },
       'content-container': {
+        gap: '0 8vw',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         flexDirection: 'column',
         py: '78px',
 
@@ -23,6 +24,7 @@ const Container = styled('div', {
         svg: {
           px: '28px',
           minWidth: '100%',
+          maxWidth: 1000,
           '@bp2': {
             position: 'relative',
             right: '-10%',
@@ -38,10 +40,10 @@ const Container = styled('div', {
 
 const Text = styled('p', {
   fontFamily: '$body',
+  fontSize: 'clamp($3, 1.35vw, 18px)',
+  lineHeight: 'clamp(25px, 1.5vw, 22px)',
   fontWeight: 500,
   color: '$black',
-  fontSize: 'clamp($4, 2vw, $6)',
-  textIndent: 'clamp(50px, 8vw, 150px)',
 
   variants: {
     font: {
@@ -50,6 +52,33 @@ const Text = styled('p', {
         fontSize: '546px',
         lineHeight: '656px',
         fontWeight: 800
+      }
+    },
+    size: {
+      bg: {
+        fontSize: 'clamp(18px, 2vw, $6)',
+        lineHeight: 'clamp(25px, 2.2vw, 39px)'
+      },
+      sm: {
+        fontSize: 14,
+        lineHeight: '20px'
+      }
+    },
+    extra: {
+      before: {
+        '&:before': {
+          display: 'none',
+          marginRight: '8px',
+          content: 'About the font',
+          fontSize: '14px',
+          lineHeight: '26px',
+          textTransform: 'uppercase',
+          verticalAlign: 'top',
+
+          '@bp2': {
+            display: 'inline'
+          }
+        }
       }
     }
   }
@@ -60,21 +89,50 @@ const AboutSection = () => {
     <Container as="section" type="section-container">
       <Container type="content-container">
         <GrostesqueAB />
-        <Container css={{ minWidth: '50%', pl: '40px' }}>
-          <Text>
-            BSMNT Grotesque is a font inspired by varius placerat urna ornare
-            hendrerit nascetur. Ac purus molestie eleifend magna turpis
-            bibendum. Porta habitant nunc integer augue nam feugiat cras aenean.
+        <Container css={{ pl: '40px', maxWidth: 900 }}>
+          <Text
+            size="sm"
+            css={{
+              fontWeight: '700',
+              ta: 'center',
+              textTransform: 'uppercase',
+              marginBottom: '48px',
+              '@bp2': { display: 'none' }
+            }}
+          >
+            About the font
           </Text>
-          <Text>
-            Etiam eget sit mattis pretium sed. Imperdiet eget urna nulla aliquet
-            urna. Volutpat in morbi quam pulvinar in magna duis. Adipiscing nisi
-            placerat mus nisl vitae imperdiet. Posuere arcu molestie diam
-            egestas. Placerat iaculis sed quis eget massa.
+          <Text size="bg" css={{ marginBottom: '12px' }} extra="before">
+            BSMNT Grotesque is the studio’s first venture into the daunting but
+            exciting world of type design. Of course, we had to start with a
+            heavy weight: striking and unapologetically so; flawed but charming
+            and full of character.
+          </Text>
+          <Text css={{ textIndent: 120 }}>
+            We set out inspired by the expressiveness of early 19th-century
+            grotesque typefaces and the boldness and striking visuals of the
+            contemporary revival of brutalist aesthetics. BSMNT is the first
+            step in a very ambitious path we’ve set for ourselves.
+          </Text>
+          <Text css={{ textIndent: 120 }}>
+            The typeface is a work in progress, open to anyone who shares our
+            visual and graphic sensibilities. You're invited to check our
+            journey as we iterate, change, and add new weights and widths in the
+            future as we learn by doing.
           </Text>
         </Container>
       </Container>
-      <Text css={{ ta: 'center', fontWeight: 800 }}>***</Text>
+      <Text
+        size="bg"
+        css={{
+          ta: 'center',
+          fontWeight: 800,
+          fontFamily: '$heading',
+          marginTop: '-24px'
+        }}
+      >
+        ***
+      </Text>
     </Container>
   )
 }
