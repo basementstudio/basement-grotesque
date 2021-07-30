cfbbf91a2b32aa72129f9ad510c480bd7d26a3bd
Nazareno Oviedo <nazareno@basement.studio>
2021-07-30 16:35:35 -0300

    feat: hero improves

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -8,10 +8,36 @@ import background from '../../../../public/images/bghero.jpg'
 
 const BackgroundImage = styled('div', {
   width: '100vw',
-  height: '90vh',
+  height: '85vh',
   position: 'fixed',
   overflow: 'hidden',
   zIndex: '-1'
+})
+
+const Title = styled('h1', {
+  color: 'white',
+  fontFamily: '$heading',
+  fontSize: '120px',
+  letterSpacing: '-1px',
+  lineHeight: '1',
+  overflow: 'hidden',
+  textAlign: 'center',
+  textTransform: 'uppercase'
+})
+
+const Outlined = styled('span', {
+  textShadow:
+    '1.5px 0 0 white, -1.5px 0 0 white, 0 1.5px 0 white, 0 -1.5px 0 white, 1.5px 1.5px white, -1.5px -1.5px 0 white, 1.5px -1.5px 0 white, -1.5px 1.5px 0 white',
+  color: '$black',
+  fontSize: '131px'
+})
+
+const Wrapper = styled('div', {
+  alignItems: 'center',
+  display: 'flex',
+  height: '85vh',
+  justifyContent: 'center',
+  paddingTop: '$3'
 })
 
 const Hero = () => (
@@ -26,6 +52,11 @@ const Hero = () => (
         src={background}
       />
     </BackgroundImage>
+    <Wrapper>
+      <Title>
+        <Outlined>Basement</Outlined> <br /> Grotesque
+      </Title>
+    </Wrapper>
   </section>
 )
 
