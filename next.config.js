b0a89e89fc02752971998f2dbd93b13b1c0514b1
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2021-07-30 10:45:17 -0300

    gsap & next-svgr installation

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 const withTM = require('next-transpile-modules')
+const withSvgr = require('next-svgr')
 
 module.exports = withPlugins(
-  [withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }), withTM([])],
-  { reactStrictMode: true }
+  [
+    withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
+    withTM(['gsap']),
+    withSvgr()
+  ],
+  {
+    reactStrictMode: true,
+    future: {
+      webpack5: true
+    }
+  }
 )
