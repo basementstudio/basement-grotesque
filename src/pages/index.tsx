0924f64e2f415f7aa23c826cdf2d2a38a27c0b37
Julian Benegas <julianbenegas99@gmail.com>
2021-07-30 16:51:36 -0300

    merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import PageLayout from 'components/layout/page'
 // Sections
 import Hero from 'components/sections/hero'
 import AboutSection from 'components/sections/about'
+import DemoSection from 'components/sections/demo'
 
 const HomePage = () => {
   return (
     <PageLayout>
       <Hero />
       <AboutSection />
+      <DemoSection />
     </PageLayout>
   )
 }
