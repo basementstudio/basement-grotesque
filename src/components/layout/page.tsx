0cda3611db5027b40916333fb663d19a8a74455f
matiasperz <matiasperezpc@hotmail.com>
2021-08-18 16:07:26 -0300

    Removing container from page layout

--- src/components/layout/page.tsx
+++ src/components/layout/page.tsx
@@ -1,11 +1,9 @@
 import Head, { HeadProps } from 'components/common/head'
-import Container, { ContainerProps } from './container'
 import Footer from './footer'
 import Header from './header'
 
 type Props = {
   children?: React.ReactNode
-  contain?: boolean | ContainerProps
   headProps?: HeadProps
 
   // TODO after implementing head, header, footer
@@ -13,14 +11,12 @@ type Props = {
   // footerProps: FooterProps
 }
 
-const PageLayout = ({ children, contain, headProps }: Props) => {
+const PageLayout = ({ children, headProps }: Props) => {
   return (
     <>
       <Head {...headProps} />
       <Header />
-      <main>
-        {contain ? <Container {...contain}>{children}</Container> : children}
-      </main>
+      <main>{children}</main>
       <Footer />
     </>
   )
