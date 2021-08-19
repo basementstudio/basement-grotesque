d882357e3f1ea09c6e6deab232f278a6d80ba14a
Julian Benegas <julianbenegas99@gmail.com>
2021-08-19 10:28:57 -0300

    reuse box

--- src/components/sections/characters/index.tsx
+++ src/components/sections/characters/index.tsx
@@ -7,8 +7,8 @@ import Section from 'components/primitives/section'
 // Styles
 import { styled } from '../../../../stitches.config'
 import toast from 'react-hot-toast'
+import Box from 'components/common/box'
 
-const Box = styled('div', {})
 const DesktopOnlyBox = styled('div', {
   display: 'none',
   '@bp3': {
@@ -96,37 +96,49 @@ const CharactersSection = () => {
   return (
     <Section>
       <SectionInner css={{ pb: viewAll ? '128px' : '0px' }}>
-        <Box
-          css={{
-            position: 'absolute'
-          }}
-        >
-          <svg
-            width="638"
-            height="810"
-            viewBox="0 0 638 810"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <Box css={{ position: 'relative' }}>
+          <Box
+            css={{
+              position: 'absolute',
+              pointerEvents: 'none',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}
           >
-            <path
-              d="M528.721 68.2758C543.566 77.5248 551.979 95.6945 554.372 120.803C556.765 145.901 553.132 177.835 543.988 214.45C525.702 287.673 485.406 379.5 427.425 472.562C369.443 565.624 304.776 642.267 247.108 690.953C218.272 715.299 191.208 732.635 167.624 741.549C144.031 750.466 124.013 750.923 109.168 741.674C94.3235 732.425 85.9114 714.255 83.5175 689.147C81.1245 664.049 84.758 632.115 93.9019 595.5C112.188 522.277 152.484 430.45 210.465 337.388C268.447 244.326 333.114 167.683 390.782 118.997C419.618 94.6513 446.682 77.3151 470.266 68.4012C493.859 59.4839 513.877 59.0269 528.721 68.2758Z"
-              stroke="white"
-            />
-          </svg>
-        </Box>
-        <Box
-          as="h3"
-          css={{
-            fontFamily: '$heading',
-            fontSize: '88px',
-            textTransform: 'uppercase',
-            fontWeight: 800,
-            wordBreak: 'break-all',
-            textAlign: 'center',
-            lineHeight: '1'
-          }}
-        >
-          ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789!?&
+            <svg
+              width="638"
+              height="810"
+              viewBox="0 0 638 810"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M528.721 68.2758C543.566 77.5248 551.979 95.6945 554.372 120.803C556.765 145.901 553.132 177.835 543.988 214.45C525.702 287.673 485.406 379.5 427.425 472.562C369.443 565.624 304.776 642.267 247.108 690.953C218.272 715.299 191.208 732.635 167.624 741.549C144.031 750.466 124.013 750.923 109.168 741.674C94.3235 732.425 85.9114 714.255 83.5175 689.147C81.1245 664.049 84.758 632.115 93.9019 595.5C112.188 522.277 152.484 430.45 210.465 337.388C268.447 244.326 333.114 167.683 390.782 118.997C419.618 94.6513 446.682 77.3151 470.266 68.4012C493.859 59.4839 513.877 59.0269 528.721 68.2758Z"
+                stroke="white"
+              />
+            </svg>
+          </Box>
+          <Box
+            as="p"
+            css={{
+              fontFamily: '$heading',
+              fontSize: '88px',
+              textTransform: 'uppercase',
+              fontWeight: 800,
+              wordBreak: 'break-all',
+              textAlign: 'center',
+              lineHeight: '1',
+              maxWidth: '1280px',
+              margin: 'auto'
+            }}
+          >
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+            <br />
+            abcdefghijklmnopqrstuvwxyz
+            <br />
+            0123456789!?&
+          </Box>
         </Box>
         <Box
           css={{
