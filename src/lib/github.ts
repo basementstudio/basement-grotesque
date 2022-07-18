e76c1c31e1ade4b0bfe686351e5ef334f67c3e1b
Benjamin Ryan <ben@ryben.dev>
2022-07-18 17:01:31 -0500

    minimize

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,7 +1,8 @@
 const GITHUB_ACCOUNT = 'basementstudio'
 const GITHUB_REPO = 'basement-grotesque'
-const RELEASE_TAG = 'latest'
-const API_ENDPOINT = `https://api.github.com/repos/${GITHUB_ACCOUNT}/${GITHUB_REPO}/releases/${RELEASE_TAG}`
+const GITHUB_TAG = 'latest'
+
+const GITHUB_SLUG = `${GITHUB_ACCOUNT}/${GITHUB_REPO}/releases/${GITHUB_TAG}`
 
 type Asset = {
   browser_download_url: string
@@ -11,10 +12,11 @@ type GitHubRes = {
   assets: Asset[]
 }
 
-export const getReleaseLink = () =>
-  `https://github.com/${GITHUB_ACCOUNT}/${GITHUB_REPO}/releases/latest`
+export const getReleaseLink = () => `https://github.com/${GITHUB_SLUG}`
 
 export const getDownloadLink = async () => {
-  const res = (await fetch(API_ENDPOINT).then((res) => res.json())) as GitHubRes
+  const res = (await fetch(`https://api.github.com/repos/${GITHUB_SLUG}`).then(
+    (res) => res.json()
+  )) as GitHubRes
   return res.assets[0]?.browser_download_url
 }
